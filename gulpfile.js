e72898e8eaaf3869be4c541dfce72841cc448fab
Rishi Katyal <rishikatyal@Rishis-MacBook-Pro.local>
2016-10-17 22:18:26 -0400

    Adding gulp commands to move html and css content from src to public, Adding public folder to gitognore

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,36 @@
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
 var merge = require('merge2');
+var del = require('del');
 
-var uiFiles = ['src/**/*.ts'];
+var tsFiles = 'src/**/*.ts';
+var htmlFiles = 'src/**/*.html';
+var cssFiles = 'src/**/*.css';
 
 
-var compileUi = ts.createProject('src/tsconfig.json', {typescript: require('typescript')});
-gulp.task('compileUi', function() {
-    var tsResult = compileUi.src().pipe(ts(compileUi));
+gulp.task('clean', function () {
+      del(['public']);
+});
+
+var compileTsFiles = ts.createProject('src/tsconfig.json', {typescript: require('typescript')});
+gulp.task('compileTsFiles', function() {
+    var tsResult = compileTsFiles.src().pipe(ts(compileTsFiles));
     return merge([
         tsResult.dts.pipe(gulp.dest('public/')),
         tsResult.js.pipe(gulp.dest('public/'))
     ]);
 });
 
-gulp.task('run', ['compileUi']);
-gulp.task('default', ['compileUi']);
+var compileHtmlFiles =[htmlFiles];
+gulp.task('compileHtmlFiles', function() {
+    return gulp.src(compileHtmlFiles)
+        .pipe(gulp.dest('public/'));
+});
+
+var compileCssFiles =[cssFiles];
+gulp.task('compileCssFiles', function() {
+    return gulp.src(compileCssFiles)
+        .pipe(gulp.dest('public/'));
+});
+
+gulp.task('run', ['clean', 'compileTsFiles', 'compileHtmlFiles', 'compileCssFiles']);
